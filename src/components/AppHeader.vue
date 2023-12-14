3249fe9900ace617632bdac1e73ab4f19fe025c2
elisabichiappini <elisabichiappini@gmail.com>
2023-12-14 12:26:25 +0100

    feat: logocomponent inside project

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,19 @@
 <script>
+//importazioni file dei componenti
+import AppLogo from './AppLogo.vue';
 //rendo esportabile il file.vue: identifico con un nome, eventuali componenti registrati, data, methods e relativi cicli di vita
 export default {
     name: 'Header',
+    components: {
+    AppLogo,
+    AppLogo
+}
 };
 </script>
 
 <template>
-    <header id="intestation" class="d-flex">
-        <div>image</div>
+    <header id="intestation" class="d-flex align-center">
+        <AppLogo></AppLogo>
         <h1>Yu-Gi-Oh API</h1>
     </header>
 </template>
---
+++ src/components/AppLogo.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: 'Logo',
+    data() {}
+}
+</script>
+
+<template>
+    <div class="box-logo">
+        <img class="img-content" src="/logo.png" alt="logo_YuGiOh">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.box-logo {
+    width: 100px;
+}
+</style>
